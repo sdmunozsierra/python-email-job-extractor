7d9dbc837e49f0f12c954a4225fcd33b7d620cdf
Sergio Sierra <sergiosierra@cleta.io>
2023-12-06 18:14:33 -0500

    latest timeline update

--- src/formatting/timeline.py
+++ src/formatting/timeline.py
@@ -1,10 +1,10 @@
 from docx import Document
-from docx.shared import Inches, Pt
+from docx.shared import Inches, Pt, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 
-def set_cell_border(cell, border_pos, border_width):
+def set_cell_border(cell, border_pos, border_width, border_color="auto"):
     """ Set border of a cell for the timeline """
     tc = cell._element
     tcPr = tc.get_or_add_tcPr()
@@ -15,31 +15,32 @@ def set_cell_border(cell, border_pos, border_width):
     border = OxmlElement(f'w:{border_pos}')
     border.set(qn('w:sz'), str(border_width))
     border.set(qn('w:val'), 'single')
-    border.set(qn('w:color'), 'auto')
+    border.set(qn('w:color'), border_color)
     borders.append(border)
 
     tcPr.append(borders)
 
-def add_timeline_section(table, positions, start_idx, end_idx, offset=0, is_top=True):
-    """ Add timeline section (top or bottom) """
-    for i in range(start_idx, end_idx):
-        col_idx = 2 * (i - start_idx) + offset
-        position = positions[i]
+def add_timeline_entry(table, position, col_idx):
+    """ Add timeline entry (title and summary) to the table """
+    # Determine the row index based on column index
+    is_even_col = col_idx % 2 == 0
+    title_row_idx = 1 if is_even_col else 2
+    summary_row_idx = 0 if is_even_col else 3
 
-        # Position title cell
-        title_cell = table.cell(0, col_idx)
-        title_p = title_cell.paragraphs[0]
-        title_p.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        run = title_p.add_run(f"{position['title']} ({position['start']} - {position['end']})")
-        run.bold = True
+    # Title cell
+    title_cell = table.cell(title_row_idx, col_idx)
+    title_paragraph = title_cell.paragraphs[0]
+    title_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    title_run = title_paragraph.add_run(f"{position['title']} ({position['start']} - {position['end']})")
+    title_run.bold = True
+    title_run.font.size = Pt(12)
 
-        # Merge with next cell for spacing, if not the last cell
-        if col_idx + 1 < len(table.columns):
-            title_cell.merge(table.cell(0, col_idx + 1))
-
-        # Add timeline border to cell
-        border_pos = 'top' if is_top else 'bottom'
-        set_cell_border(title_cell, border_pos, 4)  # Adjust border width as needed
+    # Summary cell
+    summary_cell = table.cell(summary_row_idx, col_idx)
+    summary_paragraph = summary_cell.paragraphs[0]
+    summary_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    summary_run = summary_paragraph.add_run(position['summary'])
+    summary_run.font.size = Pt(10)
 
 def add_alternating_timeline(doc, positions):
     # Title
@@ -49,16 +50,13 @@ def add_alternating_timeline(doc, positions):
     run.bold = True
     run.font.size = Pt(14)
 
-    # Calculate the number of positions for the top and bottom tables
-    half_len = len(positions) // 2
+    # Create a table for the timeline
+    table = doc.add_table(rows=4, cols=len(positions) * 2)
 
-    # Create top table for the first half of positions
-    top_table = doc.add_table(rows=1, cols=2 * half_len)
-    add_timeline_section(top_table, positions, 0, half_len, is_top=True)
+    # Add timeline entries
+    for i, position in enumerate(positions):
+        add_timeline_entry(table, position, i)
 
-    # Create bottom table for the second half of positions, offset by one cell
-    bottom_table = doc.add_table(rows=1, cols=2 * (len(positions) - half_len) + 1)
-    add_timeline_section(bottom_table, positions, half_len, len(positions), offset=1, is_top=False)
 
 # Example positions data
 positions = [
@@ -74,5 +72,4 @@ doc = Document()
 add_alternating_timeline(doc, positions)
 
 # Save the document
-doc.save('alternating_timeline_cv.docx')
-
+doc.save('professional_timeline_cv.docx')
